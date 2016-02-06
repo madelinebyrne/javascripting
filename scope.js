94dbc4b00a13a14743a8cdb04e6ba027a0f18b8c
madelinebyrne <madelinebyrne@seattleacademy.org>
2016-02-06 07:59:36 -0800

    solved scope.js

--- scope.js
+++ scope.js
@@ -1,3 +1,23 @@
+  var a = 1, b = 2, c = 3;
+
+     (function firstFunction(){
+         var b = 5, c = 6;
+
+         (function secondFunction(){
+             var b = 8;
+ console.log("a: "+a+", b: "+b+", c: "+c);
+             (function thirdFunction(){
+                 var a = 7, c = 9;
+
+                 (function fourthFunction(){
+                     var a = 1, c = 8;
+
+                 })();
+             })();
+         })();
+     })();
+ 
+
  /*
  ## The challenge:
 
@@ -12,7 +32,7 @@
 
          (function secondFunction(){
              var b = 8;
-
+		
              (function thirdFunction(){
                  var a = 7, c = 9;
 
@@ -23,7 +43,7 @@
              })();
          })();
      })();
-
+ console.log("a: "+a+", b: "+b+", c: "+c);
   Use your knowledge of the variables' scope and place the following code
   inside one of the functions in scope.js so the output is a: 1, b: 8,c: 6
 
